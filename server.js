2c7bbdbf4aa4f6eebc93de12d3196ed3da6b1676
criswithouth101 <cristina.arredondogu@gmail.com>
2025-05-27 21:56:49 +0200

    done

--- server.js
+++ server.js
@@ -22,18 +22,51 @@ app.get('/greetings/:username', (req, res) => {
   });
 
   app.get('/collectibles/:index', (req, res) => {
-    const indexParam = req.params.index;
-    const indexInt = parseInt(indexParam);
-    
     const collectibles = [
         { name: 'shiny ball', price: 5.95 },
         { name: 'autographed picture of a dog', price: 10 },
         { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
       ];
-    
+      const indexParam = req.params.index;
+      const indexInt = parseInt(indexParam);
 
-    res.send(`Hello there and welcome, ${req.params.username}!`);
+      if (Number.isNaN(indexInt) || indexInt > 0 || indexInt >= collectibles.length ) {
+        return res.send('This item is not yet in stock. Check back soon!');
+      } else { 
+        const selected = collectibles[indexInt];
+        res.send(`so, you want the ${selected.name}? for ${selected.price}, it can be yours!`); }
   });
+
+  app.get('/shoes', (req, res) => {
+    const shoes = [
+        { name: "Birkenstocks", price: 50, type: "sandal" },
+        { name: "Air Jordans", price: 500, type: "sneaker" },
+        { name: "Air Mahomeses", price: 501, type: "sneaker" },
+        { name: "Utility Boots", price: 20, type: "boot" },
+        { name: "Velcro Sandals", price: 15, type: "sandal" },
+        { name: "Jet Boots", price: 1000, type: "boot" },
+        { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+    ];
+    const minPrice = parseFloat(req.query['min-price']);
+    const maxPrice = parseFloat(req.query['max-price']);
+    const type = req.query.type;
+
+    let filteredShoes = shoes;
+
+    if (!Number.isNaN(minPrice)) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+    }
+  
+    if (!Number.isNaN(maxPrice)) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+    }
+  
+    if (type) {
+      filteredShoes = filteredShoes.filter(shoe => shoe.type === type);
+    }
+  
+    res.json(filteredShoes);
+});
 
 // Listen for requests on port 3000
 app.listen(3000, () => {
